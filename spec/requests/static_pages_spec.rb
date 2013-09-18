85a58e965bb4878b698229a958239f6f2c1974f4
梅本 久美子 <umekumi@umemoto-no-MacBook-Air.local>
2013-09-18 21:46:29 +0900

    最後

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title){'Ruby on Rails Tutorial Sample App'}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -8,7 +11,7 @@ describe "Static pages" do
     end
     it "shoul have the title 'Home" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App|Home')
+      expect(page).to have_title("#{base_title}|Home")
     end
   end
 
@@ -19,7 +22,7 @@ describe "Static pages" do
     end
     it "should have the thitle 'Help" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App|Help")
+      expect(page).to have_title("#{base_title}|Help")
     end
   end
 
@@ -30,7 +33,19 @@ describe "Static pages" do
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App|About Us")
+      expect(page).to have_title("#{base_title}|About Us")
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title}|Contact Us")
+    end
+  end
+
 end
